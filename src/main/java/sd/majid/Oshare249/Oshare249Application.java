c83ca302c20a872988bec2ddde4421cee51567ce
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2023-02-02 15:39:16 +0100

    int

---
+++ src/main/java/sd/majid/Oshare249/Oshare249Application.java
@@ -0,0 +1,13 @@
+package sd.majid.Oshare249;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oshare249Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oshare249Application.class, args);
+	}
+
+}
---
+++ src/test/java/sd/majid/Oshare249/Oshare249ApplicationTests.java
@@ -0,0 +1,13 @@
+package sd.majid.Oshare249;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oshare249ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
